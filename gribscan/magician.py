5208e6b6d08f8a03c648278c4392fa2e0eeda31b
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2025-01-20 10:48:29 +0100

    fix make compression deterministic
    - zstd is not deterministic --> lz4

--- gribscan/magician.py
+++ gribscan/magician.py
@@ -199,7 +199,7 @@ class HarmonieMagician(MagicianBase):
     def coords_hook(self, name, coords):
         dims = [name]
         attrs = {}
-        compressor = numcodecs.Blosc("zstd", shuffle=0)
+        compressor = numcodecs.Blosc("lz4", shuffle=0)
         if "time" in name:
             attrs = {
                 "units": "seconds since 1970-01-01T00:00:00",
