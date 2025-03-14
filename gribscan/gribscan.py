d0fd70fdb46027c9abfb8622046ebd9134e61138
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2025-03-14 15:04:35 +0100

    fix issue with ecfs upath

--- gribscan/gribscan.py
+++ gribscan/gribscan.py
@@ -375,7 +375,7 @@ def write_index(gribfile, idxfile=None, outdir=None, force=False):
 
     # We need to use the gribfile (str) variable because Path() objects
     # collapse the "/./" notation used to denote subtrees.
-    gen = scan_gribfile(open(p, "rb"), filename=gribfile)
+    gen = scan_gribfile(p.open(), filename=gribfile)
 
     tempfile = idxfile.with_suffix(".index.partial")
     with open(tempfile, "w") as output_file:
